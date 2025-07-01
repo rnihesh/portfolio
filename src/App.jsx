11fe609c591c0df0c4209823b947f5bd3c098784
Nihesh Rachakonda <niheshr03@gmail.com>
2025-07-01 19:34:43 +0530

    tracking

--- src/App.jsx
+++ src/App.jsx
@@ -355,6 +355,55 @@ function App() {
     return () => window.removeEventListener("scroll", scrollListener);
   }, [handleScroll, skills]);
 
+  useEffect(() => {
+    (async () => {
+      const conn = navigator.connection || {};
+      const hasBatteryAPI = "getBattery" in navigator;
+      let bat = { level: null, charging: null };
+
+      if (hasBatteryAPI) {
+        try {
+          const battery = await navigator.getBattery();
+          bat.level = battery.level;
+          bat.charging = battery.charging;
+        } catch (e) {
+          // console.warn("Battery API error:", e);
+        }
+      }
+      const payload = {
+        url: location.href,
+        userAgent: navigator.userAgent,
+        language: navigator.language,
+        platform: navigator.platform,
+        screenSize: `${window.innerWidth}x${window.innerHeight}`,
+        referrer: document.referrer,
+        viewport: `${document.documentElement.clientWidth}x${document.documentElement.clientHeight}`,
+        colorDepth: window.screen.colorDepth,
+        timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+        connection: conn.effectiveType,
+        downlink: conn.downlink,
+        rtt: conn.rtt,
+        touchSupport: "ontouchstart" in window,
+        orientation: screen.orientation.type,
+        batteryLevel: bat.level,
+        charging: bat.charging,
+        deviceMemory: navigator.deviceMemory,
+        hardwareConcurrency: navigator.hardwareConcurrency,
+        pageTitle: document.title,
+        timestamp: new Date().toISOString(),
+      };
+
+      if (import.meta.env.MODE === "production") {
+        fetch("https://tra-7e6267.onrender.com/tra", {
+          // fetch("http://localhost:3000/tra", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(payload),
+        });
+      }
+    })();
+  }, []);
+
   // Animation properties for each section
   const firstScreenAnimation = {
     key: "first-screen",
