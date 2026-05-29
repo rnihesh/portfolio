23d2db3d5848a8e94b9e0d7a8ef4eeb0480ab903
Nihesh Rachakonda <niheshr03@gmail.com>
2026-05-29 20:12:58 +0530

    update resume to ea-agentic-ai

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
           return;
         }
         e.preventDefault();
-        window.open("/resume/v6.pdf", "_blank");
+        window.open("/resume/v7.pdf", "_blank");
       }
     };
 
